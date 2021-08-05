4bc0624a0780e70fc7bff7a56d699158e9e5305a
alifgiant <alif.akbar@bukalapak.com>
2021-08-05 11:42:18 +0800

    create router

---
+++ flutter/lib/router/router.dart
@@ -0,0 +1,22 @@
+import 'package:get/get.dart';
+import 'package:recognition_example/pages/detect_face_page.dart';
+import 'package:recognition_example/pages/register_face_page.dart';
+import 'package:recognition_example/pages/root_page.dart';
+import 'package:recognition_example/router/router_name.dart';
+
+class Router {
+  static List<GetPage> pages = [
+    GetPage(
+      name: RouteName.root,
+      page: () => RootPage(),
+    ),
+    GetPage(
+      name: RouteName.register_face,
+      page: () => RegisterFacePage(),
+    ),
+    GetPage(
+      name: RouteName.detect_face,
+      page: () => DetectFacePage(),
+    ),
+  ];
+}
---
+++ flutter/lib/router/router_name.dart
@@ -0,0 +1,6 @@
+class RouteName {
+  // route
+  static const String root = "/";
+  static const String register_face = "/register-face";
+  static const String detect_face = "/detect-face";
+}
