a1a4a4c8651466d261101713b84749c30ce2a8fa
alifgiant <alif.akbar@bukalapak.com>
2021-08-05 15:27:25 +0800

    add auto close in register face

--- flutter/lib/pages/register_face_page.dart
+++ flutter/lib/pages/register_face_page.dart
@@ -1,9 +1,76 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:recognition_example/pages/camera_view.dart';
 
 class RegisterFaceController extends GetxController {
-  Future processImage(CameraResult cameraResult) async {}
+  // region process image
+  List<double> _meanFeature = [];
+  int startCompareFeature = 0;
+  bool backInProcess = false;
+  Future processImage(CameraResult cameraResult) async {
+    if (_meanFeature.isEmpty) {
+      await scheduleCalculateRecord(cameraResult.feature);
+      startCompareFeature = DateTime.now().millisecondsSinceEpoch;
+    } else {
+      // isBelowThreshold == isFaceSame
+      final isBelowThreshold = await compareMeanRecord(cameraResult.feature);
+
+      // if 3 second pass and under treshold, close cam
+      int endCompareFeature = DateTime.now().millisecondsSinceEpoch;
+      if (isBelowThreshold && endCompareFeature - startCompareFeature > 3000) {
+        if (backInProcess) return;
+        backInProcess = true;
+        Get.back(result: CameraResult(cameraResult.image, _meanFeature));
+      } else if (!isBelowThreshold) {
+        _meanFeature = [];
+      }
+    }
+  }
+
+  final List<List<double>> _features = List.empty(growable: true);
+  int startAddFeature = 0;
+  Future scheduleCalculateRecord(List<double> feature) async {
+    if (_features.isEmpty) {
+      startAddFeature = DateTime.now().millisecondsSinceEpoch;
+    }
+    _features.add(feature);
+    int endAddFeature = DateTime.now().millisecondsSinceEpoch;
+
+    // only calculate mean if has pass 1.5s
+    if (endAddFeature - startAddFeature < 1500) return;
+    _meanFeature = await calculateMean(_features);
+    _features.clear(); // reset holder
+  }
+
+  Future<List<double>> calculateMean(List<List<double>> features) async {
+    final firstItem = features.first;
+    final List<double> meanFeature = List.filled(firstItem.length, 0);
+
+    for (int j = 0; j < firstItem.length; j++) {
+      double sumItem = 0;
+      for (int i = 0; i < features.length; i++) {
+        final feature = features[i];
+        sumItem += feature[j];
+      }
+
+      meanFeature[j] = sumItem / features.length;
+    }
+
+    return meanFeature;
+  }
+
+  Future<bool> compareMeanRecord(List<double> feature) async {
+    double sumItem = 0;
+    for (int i = 0; i < _meanFeature.length; i++) {
+      final diff = _meanFeature[i] - feature[i];
+      sumItem += diff * diff;
+    }
+    final distance = sqrt(sumItem);
+    return distance < 0.1;
+  }
+  // endregion
 }
 
 class RegisterFacePage extends StatelessWidget {
--- flutter/lib/pages/root_page.dart
+++ flutter/lib/pages/root_page.dart
@@ -1,11 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:recognition_example/core/view_utils.dart';
+import 'package:recognition_example/pages/camera_view.dart';
 import 'package:recognition_example/router/router_name.dart';
 
 class RootPageController extends GetxController {
-  void goToRegisterPage() {
-    Get.toNamed(RouteName.register_face);
+  CameraResult? _cameraResult;
+  String name = 'No Face';
+
+  Future<String> askForPersonName() async {
+    String name = "";
+    await Get.dialog(
+      Dialog(
+        child: ListView(
+          padding: const EdgeInsets.all(21),
+          shrinkWrap: true,
+          children: [
+            TextField(
+              decoration: const InputDecoration(labelText: 'Person Name'),
+              textInputAction: TextInputAction.next,
+              onChanged: (t) => name = t,
+            ),
+            12.vSpace,
+            ElevatedButton(
+              onPressed: () => Get.back(),
+              child: const Text('Register Person'),
+            ),
+          ],
+        ),
+      ),
+    );
+    return name;
+  }
+
+  bool canScanFace() => _cameraResult != null;
+
+  void goToRegisterPage() async {
+    String name = await askForPersonName();
+    if (name.isNotEmpty) {
+      final camResult = await Get.toNamed(RouteName.register_face);
+      handleCamResult(name, camResult);
+    } else {
+      Get.defaultDialog(
+        title: 'Name empty',
+        middleText: 'Please fill person name',
+      );
+    }
+  }
+
+  void handleCamResult(final String name, dynamic cameraResult) {
+    if (cameraResult is! CameraResult) return;
+    _cameraResult = cameraResult;
+    this.name = name;
+    update();
   }
 
   void goToDetectPage() {
@@ -32,19 +79,20 @@ class RootPage extends StatelessWidget {
   }
 
   ListView createBody(RootPageController ctlr) {
+    final textRegister = ctlr.canScanFace() ? 'Replace' : 'Register';
     return ListView(
       padding: const EdgeInsets.all(21),
       children: [
-        const Text('No Face Registered'),
+        Text('${ctlr.name} Registered'),
         21.hSpace,
         Container(height: 21),
         ElevatedButton(
-          child: const Text('Register Face'),
+          child: Text('$textRegister Face'),
           onPressed: () => ctlr.goToRegisterPage(),
         ),
         ElevatedButton(
           child: const Text('Detect Face'),
-          onPressed: () => ctlr.goToDetectPage(),
+          onPressed: ctlr.canScanFace() ? () => ctlr.goToDetectPage() : null,
         ),
       ],
     );
